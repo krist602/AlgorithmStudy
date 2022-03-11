f450ff4b86c42eec7a9e08f5d84097b3c5f5f5df
Bell <krist602@naver.com>
2022-03-11 14:37:55 +0900

    Prog_예상 대진표.cpp

---
+++ Other/Prog_예상 대진표.cpp
@@ -0,0 +1,21 @@
+//Programmers > 2017 팁스타운 > 예상 대진표
+//https://programmers.co.kr/learn/courses/30/lessons/12985
+ 
+    //풀기 편하게 데이터만 살짝 편집해주면 되는 문제
+    //그래서 n은 왜 준걸까
+
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 1;
+    a -= 1;
+    b -= 1;
+
+    while ((a /= 2) != (b /= 2))
+        answer++;
+
+    return answer;
+}
