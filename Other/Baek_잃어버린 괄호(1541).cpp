8e3a08ba398b8e54904fa04b9d2a9f61ba417700
Bell <krist602@naver.com>
2022-03-24 05:34:57 +0900

    Baekjoon_잃어버린 괄호(1541).cpp

---
+++ Other/Baek_잃어버린 괄호(1541).cpp
@@ -0,0 +1,59 @@
+//Baekjoon > 1541 > 잃어버린 괄호
+//https://www.acmicpc.net/problem/1541
+
+	//하나라도 -가 있다면, 그 뒤에 있는 숫자들은 전부 빼줄 수 있다.
+	//이걸 생각하기가 어려웠던 문제. 
+	//푸는 방법을 안 이후에 코드 구현은 쉬웠다.
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	string str = "";
+	int s = 0, sum = 0;
+	bool flag = true;
+	vector<int> nums;
+	vector<bool> oper; //연산자가 +라면 true를, -라면 false를 집어넣었다.
+	cin >> str;
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str.at(i) == '+') //연산자를 만난다면 그 직전까지가 숫자다.
+		{
+			oper.push_back(true);
+			nums.push_back(stoi(str.substr(s, i)));
+			s = i + 1;
+		}
+		else if (str.at(i) == '-')
+		{
+			oper.push_back(false);
+			nums.push_back(stoi(str.substr(s, i)));
+			s = i + 1;
+		}
+	}
+	nums.push_back(stoi(str.substr(s)));
+
+	for (int i = 0; i < oper.size(); i++)
+	{
+		sum += nums[i];
+		if (!oper[i]) // '-' 라면
+		{
+			for (int j = i + 1; j < nums.size(); j++)
+				sum -= nums[j];
+			flag = false;
+			break;
+		}
+	}
+	if (flag)
+		sum += *nums.rbegin();
+	cout << sum << "\n";
+
+	return 0;
+}
